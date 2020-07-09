c85dd9b3109bebb6d834f4ed76736146f139bd68
reno <rketzner@knights.ucf.edu>
2020-07-09 17:43:32 -0400

    Created a plotting utility and started GMAT test cases.
    * Added util folder to orbitpy directory. This folder was created to store
      useful utility programs to aid with orbitPy development.
    
    * Created plot.py, a Python command line utility for plotting orbit state data
      output from GMAT and orbitPy, or comparing the two. Current features:
    
    	* Command line interface with help page documenting useage
    	* Automatic detection of GMAT or orbitPy data for accurately named plots
    	* Plotting of x,y, or z inertial position vs time
    	* Plotting of difference in x,y or z position vs time between two files
    
    * Added GMAT folder to OrbitProCovGrid test folder. This folder will hold GMAT
      output data needed to run propagation test cases.
    
    * Added a GMAT test case, and saved all input/output files associated with the
      test case.
    
    * Added first test case in test_OrbitPropCovGrid_4.py. It properly generates and
      outputs data that compares nicely with the corresponding GMAT data, but is
      currently set to automatically assert true until a tolerance is decided upon.
    
    * Fixed typo in .gitignore to keep test data from being tracked.
    
    * Changed example5 eccentricity to 0.

---
+++ orbitpy/tests/orbitpropcov/OrbitPropCovGrid/test_OrbitPropCovGrid_4.py
@@ -0,0 +1,103 @@
+""" *Unit tests for :class:`orbitpy.orbitpropcov.OrbitPropCovGrid` covering checks on format of produced output files and
+   propagation states.*
+"""
+'''
+   :code:`/temp/` folder contains temporary files produced during the run of the tests below. Some of the parameters are chosen
+   randomly for the tests (and compared with corresponding outputs), hence each test is run with different inputs, and expected 
+   outputs. 
+'''
+
+import unittest
+import numpy as np
+import pandas as pd
+import sys, os, shutil
+import random
+import subprocess
+import copy
+
+from orbitpy.orbitpropcov import OrbitPropCovGrid
+from orbitpy.util import PropagationCoverageParameters, CoverageCalculationsApproach
+
+class TestOrbitPropCovGrid(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        # Create new directory to store output of all the class functions. 
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        out_dir = os.path.join(dir_path, 'temp')
+        if os.path.exists(out_dir):
+            shutil.rmtree(out_dir)
+        os.makedirs(out_dir)
+        
+        # create a default set of propagation and coverage parameters specs
+        self.default_pcp = PropagationCoverageParameters(
+                        sat_id="1", 
+                        epoch="2018,5,26,12,0,0", # JD: 2458265.00000
+                        sma=7000, 
+                        ecc=0, 
+                        inc=0, 
+                        raan=0, 
+                        aop=0, 
+                        ta=0, 
+                        duration=1.0, 
+                        cov_grid_fl=dir_path+"/temp/covGrid", 
+                        sen_fov_geom="CONICAL", 
+                        sen_orien="1,2,3,0,0,0",
+                        sen_clock="0", 
+                        sen_cone="12.5", 
+                        purely_sidelook = 0, 
+                        yaw180_flag = 0, 
+                        step_size = 1.0, 
+                        sat_state_fl = dir_path+"/temp/state", 
+                        sat_acc_fl = dir_path+"/temp/acc", 
+                        cov_calcs_app= CoverageCalculationsApproach.GRIDPNTS)
+        
+        # store directory path
+        self.dir_path = dir_path
+        
+        super(TestOrbitPropCovGrid, self).__init__(*args, **kwargs)
+        
+        
+    def produce_cov_grid(self, sat_id, latUpper, latLower, lonUpper, lonLower, grid_res):
+        """ Write a coverage grid file. """
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        prc_args = [os.path.join(dir_path, '..', '..', '..', 'oci', 'bin', 'genCovGrid')] 
+
+        cov_grid_fl = dir_path + "/temp/covGrid" # coverage grid file path        
+        prc_args.append(cov_grid_fl)
+
+        prc_args.append(str(sat_id)+','+str(latUpper)+','+str(latLower)+
+                            ','+str(lonUpper)+','+str(lonLower)+','+str(grid_res) # same grid resolution for all regions
+                            )
+        result = subprocess.run(prc_args, check= True)
+        
+    def orbitpyStateArray(self,sat_state_fl):
+        
+        data = np.genfromtxt(sat_state_fl, delimiter=",",skip_header = 5) # 5th row header, 6th row onwards contains the data
+        return data
+    
+    def gmatStateArray(self,sat_state_fl):
+        
+        data = np.genfromtxt(sat_state_fl, skip_header = 2)
+        return data
+        
+    def test_run_1(self):
+        """ Test propagation of 7000 sma orbit w/ all other kepler states = 0.""" 
+        
+        TestOrbitPropCovGrid.produce_cov_grid(self, 1, 0, -10, 10, 0, 1)
+
+        prop_cov_param = copy.deepcopy(self.default_pcp)
+        opc_grid = OrbitPropCovGrid(prop_cov_param)
+        opc_grid.run()
+        
+        gmat_sat_state_fl = self.dir_path + "/GMAT/01/states.txt"
+        
+        # read in state data to numpy array
+        orbitpyData = TestOrbitPropCovGrid.orbitpyStateArray(self,self.default_pcp.sat_state_fl)
+        gmatData = TestOrbitPropCovGrid.gmatStateArray(self,gmat_sat_state_fl)
+        
+        self.assertEqual(1,1)
+        
+if __name__ == '__main__':
+    unittest.main()
+        
+        
---
+++ orbitpy/util/plot.py
@@ -0,0 +1,180 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+def orbitpyStateArray(sat_state_fl):
+    
+    data = np.genfromtxt(sat_state_fl, delimiter=",",skip_header=5)
+    return data
+
+def gmatStateArray(sat_state_fl):
+        
+    data = np.genfromtxt(sat_state_fl, skip_header = 1)
+    return data    
+
+def setPlotStyle(program='Orbitpy'):
+    
+    if program == 'OrbitPy':
+        plt.title('ECI Data (OrbitPy)')
+    elif program == 'GMAT':
+        plt.title('ECI Data (GMAT)')
+
+def plotX(data,program='OrbitPy'):
+    t = data[:,0]
+    x = data[:,1]
+    
+    plt.figure()
+    lines = plt.plot(t,x)
+    
+    setPlotStyle(program)
+    plt.setp(lines,color = 'r')
+    plt.xlabel('time (s)')
+    plt.ylabel('X (km)')
+
+def plotY(data,program='OrbitPy'):
+    t = data[:,0]
+    x = data[:,2]
+    
+    plt.figure()
+    lines = plt.plot(t,x)
+    
+    plt.setp(lines,color = 'b')
+    plt.xlabel('time (s)')
+    plt.ylabel('Y (km)')
+
+    if program == 'OrbitPy':
+        plt.title('ECI Data (OrbitPy)')
+    elif program == 'GMAT':
+        plt.title('ECI Data (GMAT)')
+
+
+def plotZ(data,program='OrbitPy'):
+    t = data[:,0]
+    x = data[:,3]
+    
+    plt.figure()
+    lines = plt.plot(t,x)
+    
+    setPlotStyle(program)
+    plt.setp(lines,color = 'g')
+    plt.xlabel('time (s)')
+    plt.ylabel('Z (km)')
+    
+    
+def plotDx(data,program='OrbitPy'):
+    t = data[:,0]
+    x = data[:,1]
+    
+    plt.figure()
+    lines = plt.plot(t,x)
+    
+    plt.title("X Difference Between States")
+    plt.setp(lines,color = 'r')
+    plt.xlabel('time(s)')
+    plt.ylabel('X Diff (km)')
+
+def plotDy(data,program='OrbitPy'):
+    t = data[:,0]
+    x = data[:,2]
+    
+    plt.figure()
+    lines = plt.plot(t,x)
+    
+    plt.title("Y Difference Between States")
+    plt.setp(lines,color = 'b')
+    plt.xlabel('time (s)')
+    plt.ylabel('Y Diff (km)')
+
+
+def plotDz(data,program='OrbitPy'):
+    t = data[:,0]
+    x = data[:,3]
+    
+    plt.figure()
+    lines = plt.plot(t,x)
+    
+    plt.title("Z Difference Between States")
+    plt.setp(lines,color = 'g')
+    plt.xlabel('time (s)')
+    plt.ylabel('Z Diff (km)')
+    
+    
+def detectHeader(sat_state_fl):
+    
+    file = open(sat_state_fl,'r')
+    
+    line = file.readline()
+    lineCount = 0
+    
+    while any(c.isalpha() for c in line):
+        lineCount = lineCount + 1
+        line = file.readline()
+    
+    file.close()
+    return lineCount
+        
+
+# Begin command line parser execution
+msg = "A simple command line utility to plot orbit state data associated with GMAT and orbitPy. \
+The orbit states of a single file can be plotted, or the states of two files compared. The file \
+source will be determined automatically based on the size of the header."
+
+parser = argparse.ArgumentParser(description=msg)
+
+parser.add_argument('path', metavar='F', nargs='+',type=str, help = 'Relative path to a text file containing orbit state data. A max of two files can be specified.')
+parser.add_argument('-x', action='store_true',help='Plot the x position of the orbit/s as a function of time.')
+parser.add_argument('-y', action='store_true',help='Plot the y position of the orbit/s as a function of time.')
+parser.add_argument('-z', action='store_true',help='Plot the z position of the orbit/s as a function of time.')
+parser.add_argument('-dx',action='store_true',help='Plot the difference between the x positions of the two inputs as a function of time.')
+parser.add_argument('-dy',action='store_true',help='Plot the difference between the y positions of the two inputs as a function of time.')
+parser.add_argument('-dz',action='store_true',help='Plot the difference between the z positions of the two inputs as a function of time.')
+
+args = parser.parse_args()
+
+## Begin program execution
+
+# define header sizes
+GMAT_SIZE = 1
+ORBITPY_SIZE = 5
+
+numHeaderLines = []
+data = []
+program = []
+
+# Loop through all (max 2) input files to generate numpy arrays
+for i in range(len(args.path)):
+    ## Generate and identify numpy arrays
+    numHeaderLines.append(detectHeader(args.path[i]))
+
+    # if OrbitPy
+    if numHeaderLines[i] == ORBITPY_SIZE:
+        data.append(orbitpyStateArray(args.path[i]))
+        program.append('OrbitPy')
+    # elif GMAT
+    elif numHeaderLines[i] == GMAT_SIZE:
+        data.append(gmatStateArray(args.path[i]))
+        program.append('GMAT')
+
+# Plot positions as requested
+for i in range(len(args.path)):
+    if args.x == True:
+        plotX(data[i],program[i])
+    
+    if args.y == True:
+        plotY(data[i],program[i])
+        
+    if args.z == True:
+        plotZ(data[i],program[i])
+
+diffData = data[0]
+diffData[:,1:5] = data[0][:,1:5] - data[1][:,1:5]
+
+# Plot differences as requested
+if args.dx == True:
+    plotDx(diffData)
+if args.dy == True:
+    plotDy(diffData)
+if args.dz == True:
+    plotDz(diffData)
+    
+plt.show()
