cfe10fdc95f379db2c011066684531d958a46ce3
vinay0000 <43803757+vinay0000@users.noreply.github.com>
2021-01-01 21:41:15 -0800

    update docs config

--- orbitpy/docs/conf.py
+++ orbitpy/docs/conf.py
@@ -38,9 +38,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.autodoc',
-    'sphinxcontrib.plantuml',
-    'sphinxcontrib.needs'
+    'sphinx.ext.autodoc'
     ]
 
 mathjax_path="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
