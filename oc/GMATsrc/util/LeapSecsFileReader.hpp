6eeec10556671c0d50e7f6be14a9f5792919ec8d
vinay0000 <43803757+vinay0000@users.noreply.github.com>
2021-01-15 16:26:10 -0800

    Revert "remove windows compilation related stuff"
    This reverts commit d1436583bc9d06d2a8b5e0bc921ef813b35c724e.

--- oc/GMATsrc/util/LeapSecsFileReader.hpp
+++ oc/GMATsrc/util/LeapSecsFileReader.hpp
@@ -1,3 +1,34 @@
+//$Id$
+//------------------------------------------------------------------------------
+//                              LeapSecsFileReader
+//------------------------------------------------------------------------------
+// GMAT: General Mission Analysis Tool.
+//
+// Copyright (c) 2002 - 2018 United States Government as represented by the
+// Administrator of the National Aeronautics and Space Administration.
+// All Other Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// You may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at:
+// http://www.apache.org/licenses/LICENSE-2.0. 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied.   See the License for the specific language
+// governing permissions and limitations under the License.
+//
+// Author: Allison R. Greene
+// Created: 2005/01/26
+//
+/**
+ * Reads time coefficent file, creates a table of coefficients and converts
+ * to the utc.
+ *
+ * File found at : ftp://maia.usno.navy.mil/ser7/tai-utc.dat
+ */
+//------------------------------------------------------------------------------
+
 #ifndef LeapSecsFileReader_hpp
 #define LeapSecsFileReader_hpp
 
@@ -18,6 +49,12 @@ struct LeapSecondInformation
    Real offset2;
    Real offset3;
 };    
+
+// Required for Visual Studio dll exports
+#ifdef EXPORT_TEMPLATES
+   EXPIMP_TEMPLATE template class DECLSPECIFIER std::allocator<LeapSecondInformation>;
+   EXPIMP_TEMPLATE template class DECLSPECIFIER std::vector<LeapSecondInformation>;
+#endif
 
 class GMATUTIL_API LeapSecsFileReader
 {
