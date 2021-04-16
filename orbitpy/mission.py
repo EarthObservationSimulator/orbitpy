09b934caddee6289da7bc0f59aac1cf653d51ca6
vinay0000 <43803757+vinay0000@users.noreply.github.com>
2021-04-16 12:22:10 -0700

    wip: mission module

--- orbitpy/mission.py
+++ orbitpy/mission.py
@@ -16,8 +16,9 @@ from instrupy.util import Entity
 from instrupy import Instrument
 
 import orbitpy.util
-from .util import Spacecraft, GroundStation
-from .propagator import PropagatorFactory, J2AnalyticalPropagator
+from .util import Spacecraft, GroundStation, SpacecraftBus
+from .constellation import ConstellationFactory
+from .propagator import PropagatorFactory
 from .coveragecalculator import CoverageCalculatorFactory, CoverageType, GridCoverage, PointingOptionsCoverage, PointingOptionsWithGridCoverage
 from datametricscalculator import DataMetricsCalculator
 from .contactfinder import ContactFinder
@@ -51,6 +52,10 @@ class Mission(Entity):
     :ivar coverageType: Type of coverage calculation. 
     :vartype coverageType: :class:`orbitpy.coveragecalculator.CoverageType` or None
 
+    :ivar fieldOfRegardCoverage: Flag to indicate if coverage calculations is to be done for field-of-regard or field-of-view.
+                                 Note that the flag is relevant only in case of `CoverageType.GRIDCOVERAGE`. . Default is None.
+    :vartype fieldOfRegardCoverage: bool or None
+
     :ivar grid: Grid object to be used in case of coverage calculations.
     :vartype grid: :class:`orbitpy.grid.Grid` or None
 
@@ -61,13 +66,15 @@ class Mission(Entity):
     :vartype _id: str
 
     """
-    def __init__(self, startDate=None, duration=None, outDir=None, spacecraft=None, propagator=None, coverageType=None, grid=None, groundStation=None, _id=None):
+    def __init__(self, startDate=None, duration=None, outDir=None, spacecraft=None, propagator=None, coverageType=None, 
+                 fieldOfRegardCoverage=None, grid=None, groundStation=None, _id=None):
         self.startDate = float(startDate) if startDate is not None else None
         self.duration = float(duration) if duration is not None else None
         self.outDir = str(outDir) if outDir is not None else None
         self.spacecraft = orbitpy.util.initialize_object_list(spacecraft, Spacecraft)
         self.propagator = propagator if propagator is not None and isinstance(propagator, J2AnalyticalPropagator) else None
         self.coverageType = coverageType if coverageType is not None and isinstance(coverageType, CoverageType) else None
+        self.fieldOfRegardCoverage = bool(fieldOfRegardCoverage) if fieldOfRegardCoverage is not None and isinstance(fieldOfRegardCoverage, bool) else None
         self.grid = grid if grid is not None and isinstance(grid, Grid) else None
         self.groundStation = orbitpy.util.initialize_object_list(groundStation, GroundStation)
 
@@ -85,11 +92,29 @@ class Mission(Entity):
 
         """
         # parse spacecraft(s) 
-        spacecraft = orbitpy.util.dictionary_list_to_object_list(d.get("Spacecraft", None), Spacecraft)
+        custom_spc_dict = d.get("spacecraft", None)
+        constel_dict = d.get("constellation", None)
+        if custom_spc_dict is not None:
+            spacecraft = orbitpy.util.dictionary_list_to_object_list(custom_spc_dict, Spacecraft)
+        elif constel_dict is not None:
+            factory = PropagatorFactory()        
+            constel = factory.get_propagator(constel_dict)
+            orbits = constel.from_dict(constel_dict).generate_orbits()
+            # one common spacecraft-bus specifications can be expected to be defined for all the satellites in the constellation
+            spc_bus_dict = d.get("spacecraftBus", None)
+            spc_bus = SpacecraftBus.from_dict(spc_bus_dict) if spc_bus_dict is not None else None
+            # list of instrument specifications could be present, in which case all the instruments in the list are distributed to all the 
+            # spacecraft in the constellation.
+            instru_dict = d.get("instrument", None)
+            instru_list = orbitpy.util.dictionary_list_to_object_list(instru_dict, Instrument) # list of instruments
+            spacecraft = []
+            for orb in orbits:
+                spacecraft.append(Spacecraft(orbitState=orb, spacecraftBus=spc_bus, instrument=instru_list))
+
         # parse propagator
         prop_dict = d.get('propagator', None)
         if prop_dict:
-            prop_type = prop_dict.get('@type', 'J2 ANALYTICAL PROPAGATOR')
+            prop_type = prop_dict.get('@type', 'J2 ANALYTICAL PROPAGATOR') # default is J2 analytical propagator
             factory = PropagatorFactory()        
             propagator = factory.get_propagator(prop_type)
         else:
@@ -110,11 +135,11 @@ class Mission(Entity):
                        spacecraft = spacecraft,
                        propagator = propagator,
                        coverageType = coverageType,
+                       fieldOfRegardCoverage = d.get('fieldOfRegardCoverage', None),
                        grid = grid,
                        groundStation = groundStation,
                        _id = d.get('@id', None)
                       ) 
-
     
     def to_dict(self):
         """ Translate the Mission object to a Python dictionary such that it can be uniquely reconstructed back from the dictionary.
@@ -129,12 +154,104 @@ class Mission(Entity):
                      "spacecraft": orbitpy.util.object_list_to_dictionary_list(self.spacecraft),
                      "propagator": self.propagator.to_dict(),
                      "coverageType": self.coverageType.value,
-                     "grid": self.grid.to_dict,
+                     "fieldOfRegardCoverage": self.fieldOfRegardCoverage,
+                     "grid": self.grid.to_dict,                     
                      "groundStation": orbitpy.util.object_list_to_dictionary_list(self.groundStation),
                      "@id": self._id})
 
     def __repr__(self):
-        pass
+        return "Mission.from_dict({})".format(self.to_dict())
 
     def execute(self):
-        pass
+        """ Execute a mission where all the spacecrafts are propagated, coverage calculated for all spacecraft-instrument-mode, 
+            ground-station contact-periods computed between all spacecraft-ground-station pairs and intersatellite contact-periods
+            computed for all possible spacecraft pairs.
+
+        :return: Mission output info as an heterogenous list of output info objects from propagation, coverage and contact calculations.
+        :rtype: list, :class:`orbitpy.propagate.PropagatorOutputInfo`, :class:`orbitpy.coveragecalculator.CoverageOutputInfo`, :class:`orbitpy.contactfinder.ContactFinderOutputInfo`
+
+        """           
+
+        out_info = [] # list to accululate info objects of the various objects        
+
+        # execute orbit propagation for all satellites in the mission
+        for spc_idx, spc in enumerate(self.spacecraft):
+            
+            # make satellite directory
+            sat_dir = self.outDir + '/' + str(spc_idx) + '/'
+            if os.path.exists(sat_dir):
+                shutil.rmtree(sat_dir)
+            os.makedirs(sat_dir)
+
+            state_cart_file = sat_dir + 'state_cartesian.csv'
+            state_kep_file = sat_dir + 'state_keplerian.csv'
+            x = self.propagator.execute(spc, self.startDate, state_cart_file, state_kep_file, self.duration)
+            out_info.append(x)
+
+            # loop over all the instruments and modes (per instrument) and calculate the corresponding coverage, data-metrics
+            for instru_idx, instru in enumerate(spc.instrument):
+
+                for mode_idx, mode in enumerate(instru.mode):
+
+                    acc_fl = sat_dir + 'access_instru' + str(instru_idx) + '_mode' + str(mode_idx) + '.csv'
+                    
+                    if self.coverageType == CoverageType.GRID_COVERAGE:
+                        cov_calc = GridCoverage(grid=self.grid, spacecraft=spc, state_cart_file=state_cart_file)
+                        x = cov_calc.execute(instru_id=instru._id, mode_id=mode._id, use_field_of_regard=self.fieldOfRegardCoverage, out_file_access=acc_fl)
+                        out_info.append(x)                       
+
+                    elif self.coverageType == CoverageType.POINTING_OPTIONS_COVERAGE:
+                        cov_calc = PointingOptionsCoverage(spacecraft=spc, state_cart_file=state_cart_file)
+                        x = cov_calc.execute(instru_id=instru._id, mode_id=mode._id, out_file_access=acc_fl)
+                        out_info.append(x) 
+
+                    elif self.coverageType == CoverageType.POINTING_OPTIONS_WITH_GRID_COVERAGE:
+                        cov_calc = PointingOptionsWithGridCoverage(grid=self.grid, spacecraft=spc, state_cart_file=state_cart_file)
+                        x = cov_calc.execute(instru_id=instru._id, mode_id=mode._id, out_file_access=acc_fl)
+                        out_info.append(x)
+                    
+                    dm_file = sat_dir + 'datametrics_instru' + str(instru_idx) + '_mode' + str(mode_idx) + '.csv'
+
+                    dm_calc = DataMetricsCalculator(spacecraft=spc, state_cart_file=state_cart_file, access_file_info=acc_fl)
+        
+                    x = dm_calc.execute(out_datametrics_fl=dm_file, instru_id=instru._idx, mode_id=mode._id)
+                    out_info.append(x)
+        
+            # loop over all the ground-stations and calculate contacts
+            for gnd_stn_idx, gnd_stn in enumerate(self.groundStation):
+                
+                out_gnd_stn_file = sat_dir+'gndStn'+str(gnd_stn_idx)+'.csv'
+                x = ContactFinder.execute(spc, gnd_stn, sat_dir, state_cart_file, None, out_gnd_stn_file, ContactFinder.OutType.INTERVAL, 0)
+                out_info.append(x)
+
+        # Calculate inter-satellite contacts
+        intersat_comm_dir = self.outDir + '/comm/'
+        if os.path.exists(intersat_comm_dir):
+            shutil.rmtree(intersat_comm_dir)
+        os.makedirs(intersat_comm_dir)
+
+        for spc1_idx in range(0, len(self.spacecraft)):
+            spc1 = self.spacecraft[spc1_idx]
+            spc1_state_cart_file = self.outDir + 'sat' + str(spc1_idx) + '/state_cartesian.csv'
+            
+            # loop over the rest of the spacecrafts in the list (i.e. from the current spacecraft to the last spacecraft in the list)
+            for spc2_idx in range(spc1_idx+1, len(self.spacecraft)):
+                
+                spc2_state_cart_file = self.outDir + 'sat' + str(spc1_idx) + '/state_cartesian.csv'
+                
+                spc2 = self.spacecraft[spc2_idx]
+                out_intersat_filename = 'sat'+str(spc_idx)+'_to_sat'+str(spc2_idx)+'.csv'
+                x = ContactFinder.execute(spc1, spc2, intersat_comm_dir, spc1_state_cart_file, spc2_state_cart_file, out_intersat_filename, ContactFinder.OutType.INTERVAL, 0)
+                out_info.append(x)
+    
+        return out_info
+                    
+
+                    
+
+
+
+
+
+
+        
--- tests/test_mission.py
+++ tests/test_mission.py
@@ -6,14 +6,14 @@ The truth data is present in the folder ``test_data``.
 
 **Tests:**
 
-* ``test_execute_1``: 1 satellite, no instrument ; propagation only.
-* ``test_execute_2``: 1 satellite, 1 instrument ; propagation, grid-coverage, data-metrics calculation.
-* ``test_execute_3``: 1 satellite, 1 instrument, 1 ground-station ; propagation, pointing-options coverage, data-metrics calculation, contact-finder (ground-station only).
-* ``test_execute_4``: 1 satellite, 1 instrument, 1 ground-station ; propagation, pointing-options with grid-coverage, data-metrics calculation, contact-finder (ground-station only).
-* ``test_execute_5``: 1 satellite, multiple instruments, multiple ground-stations ; propagation, grid-coverage, data-metrics calculation, contact-finder (ground-station only).
-* ``test_execute_6``: Multiple satellites from constellation, 1 instrument each ; propagation, pointing-options coverage, data-metrics calculation, contact-finder (inter-satellite only).
-* ``test_execute_7``: Multiple satellites from constellation, multiple-instruments per satellite ; propagation, pointing-options with grid-coverage, data-metrics calculation, contact-finder (inter-satellite only).
-* ``test_execute_8``: Multiple satellites from list, multiple instruments per satellite, multiple ground-stations ; propagation, grid-coverage, data-metrics calculation, contact-finder (ground-station and inter-satellite).
+* ``test_scenario_1``: 1 satellite, no instrument ; propagation only.
+* ``test_scenario_2``: 1 satellite, 1 instrument ; propagation, grid-coverage, data-metrics calculation.
+* ``test_scenario_3``: 1 satellite, 1 instrument, 1 ground-station ; propagation, pointing-options coverage, data-metrics calculation, contact-finder (ground-station only).
+* ``test_scenario_4``: 1 satellite, 1 instrument, 1 ground-station ; propagation, pointing-options with grid-coverage, data-metrics calculation, contact-finder (ground-station only).
+* ``test_scenario_5``: 1 satellite, multiple instruments, multiple ground-stations ; propagation, grid-coverage, data-metrics calculation, contact-finder (ground-station only).
+* ``test_scenario_6``: Multiple satellites from constellation, 1 instrument each ; propagation, pointing-options coverage, data-metrics calculation, contact-finder (inter-satellite only).
+* ``test_scenario_7``: Multiple satellites from constellation, multiple-instruments per satellite ; propagation, pointing-options with grid-coverage, data-metrics calculation, contact-finder (inter-satellite only).
+* ``test_scenario_8``: Multiple satellites from list, multiple instruments per satellite, multiple ground-stations ; propagation, grid-coverage, data-metrics calculation, contact-finder (ground-station and inter-satellite).
 
 """
 import json
@@ -28,8 +28,5 @@ from orbitpy.mission import Mission
 
 class TestMission(unittest.TestCase):
 
-    def test_from_dict(self):
-        pass
-
-    def test_execute_1(self):
+    def test_scenario_1(self):
         pass
