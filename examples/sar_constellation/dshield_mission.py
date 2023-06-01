40dd83a7bae8c1fb0223672e2432c73545d92734
vinay0000 <vinay.rrgl@gmail.com>
2023-05-31 18:13:23 -0700

    reverting to use line_terminator option, except in coveragecalculator.py

--- examples/sar_constellation/dshield_mission.py
+++ examples/sar_constellation/dshield_mission.py
@@ -291,7 +291,7 @@ def assign_pointing_bins(datametrics_fp, swath_width, spc_acc_fp):
     spc_df = spc_df.loc[spc_df['pOpts'].apply(len)>0] 
 
     with open(spc_acc_fp, 'w') as f2:
-        spc_df.to_csv(f2, index=False, header=True, lineterminator='\n', sep="\t", quoting=csv.QUOTE_NONE, quotechar="", escapechar="") # , quoting=csv.QUOTE_NONE, quotechar="",  escapechar="\\"
+        spc_df.to_csv(f2, index=False, header=True, line_terminator='\n', sep="\t", quoting=csv.QUOTE_NONE, quotechar="", escapechar="") # , quoting=csv.QUOTE_NONE, quotechar="",  escapechar="\\"
     
 ############################ Start mission simulation ############################
 
